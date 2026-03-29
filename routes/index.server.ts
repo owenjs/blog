53744671ddbb41e6bb802a5d21e95562e2f86960
Owen Evans <owen@Owens-MacBook-Pro.local>
2026-03-29 19:48:59 +0100

    relative news/ link

--- routes/index.server.ts
+++ routes/index.server.ts
@@ -7,13 +7,13 @@ export const GET = () =>
       title: "Home",
       children: html`
         <nav>
-          <p>👉 <a href="/news/">News</a></p>
+          <p>👉 <a href="news/">News</a></p>
         </nav>
 
         <h1>Common HTML elements</h1>
         <p>
-          Let's go through the most important HTML elements to
-          structure your content:
+          Let's go through the most important HTML elements to structure your
+          content:
         </p>
 
         <h2>Paragraphs</h2>
@@ -21,33 +21,29 @@ export const GET = () =>
 
         <h2>Headings</h2>
         <p>
-          At the very top of the body, we have the heading of this page
-          in an h1 element. This is what search engines (like Google)
-          and screen readers (used by visually impaired readers) look for
-          when they want to know what the page's title is. Therefore, you
-          should only ever have one h1 element on any given page.
+          At the very top of the body, we have the heading of this page in an h1
+          element. This is what search engines (like Google) and screen readers
+          (used by visually impaired readers) look for when they want to know
+          what the page's title is. Therefore, you should only ever have one h1
+          element on any given page.
         </p>
         <p>
-          The h2 element is a sub-heading. HTML has h1 up to h6 elements,
-          to mark progressively deeper nested sub-headings. You should
-          use those to mark the structure of your page. All headings
-          together should act like a table of contents for your page.
+          The h2 element is a sub-heading. HTML has h1 up to h6 elements, to
+          mark progressively deeper nested sub-headings. You should use those to
+          mark the structure of your page. All headings together should act like
+          a table of contents for your page.
         </p>
 
         <h2>Lists</h2>
         <p>
-          Let's add an ordered list
-          (meaning the list markers will be numbers):
+          Let's add an ordered list (meaning the list markers will be numbers):
         </p>
         <ol>
           <li>list item one</li>
           <li>list item two</li>
           <li>list item three</li>
         </ol>
-        <p>
-          and an unordered list
-          (the list markers will be bullet points):
-        </p>
+        <p>and an unordered list (the list markers will be bullet points):</p>
         <ul>
           <li>list item one</li>
           <li>list item two</li>
@@ -56,32 +52,32 @@ export const GET = () =>
 
         <h2>Formatting</h2>
         <p>
-          Note how all elements introduced so far cause a line-break
-          before and after them? That's because they are so-called
-          <a href="https://developer.mozilla.org/en-US/docs/Glossary/Block-level_content">block elements</a>.
+          Note how all elements introduced so far cause a line-break before and
+          after them? That's because they are so-called
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Glossary/Block-level_content"
+            >block elements</a
+          >.
         </p>
         <p>
           However, links (like the a element we just saw),
           <em>emphasis</em> (rendered as italics), and
-          <strong>strong emphasis</strong> (rendered bold),
-          are all inline elements. That means they don't cause
-          any line-breaks by default.
+          <strong>strong emphasis</strong> (rendered bold), are all inline
+          elements. That means they don't cause any line-breaks by default.
         </p>
 
         <h2>An image</h2>
-        <img src="chair.jpg" alt="A chair" height="300">
+        <img src="chair.jpg" alt="A chair" height="300" />
+        <p>We will add an image file <code>chair.jpg</code> later.</p>
         <p>
-          We will add an image file <code>chair.jpg</code> later.
+          For now, note the alt attribute on the image. It is required and
+          contains "alternative text" that is read to visually impaired readers,
+          or shown if the image fails to load.
         </p>
         <p>
-          For now, note the alt attribute on the image. It is required
-          and contains "alternative text" that is read to visually
-          impaired readers, or shown if the image fails to load.
-        </p>
-        <p>
-          If the image is relevant content, the alt text should
-          therefore be a brief description of what's in the image.
-          If the image is just decoration, you should use alt="".
+          If the image is relevant content, the alt text should therefore be a
+          brief description of what's in the image. If the image is just
+          decoration, you should use alt="".
         </p>
       `,
     }),
